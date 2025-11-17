becf2e5a1f00b031caba2242b493bd73362bef16
shreyas777-coder <90862879+shreyas777-coder@users.noreply.github.com>
2025-11-17 23:56:00 +0530

    Add files via upload

--- recipe-agent-app/agent.py
+++ recipe-agent-app/agent.py
@@ -1,6 +1,6 @@
 import os
 from google.adk.agents import Agent
-from google.adk.models.gemini import Gemini
+#from google.adk.models.gemini import Gemini
 
 # 1. Configure the LLM
 # The ADK will automatically look for the GEMINI_API_KEY environment variable.
